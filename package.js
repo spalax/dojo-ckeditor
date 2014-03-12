95ced176f846514e0e043f4dcafecddc04b5da95
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2014-03-12 20:11:12 +0200

    Added files required for dojo-build system, to process module correctly.

---
+++ package.js
@@ -0,0 +1,23 @@
+var miniExcludes = {
+		"dojo-ckeditor/README.md": 1
+	},
+	amdExcludes = {
+	},
+	isJsRe = /\.js$/,
+	isTestRe = /\/test\//;
+
+var profile = {
+	resourceTags: {
+		test: function(filename, mid){
+			return isTestRe.test(filename);
+		},
+
+		miniExclude: function(filename, mid){
+			return isTestRe.test(filename) || mid in miniExcludes;
+		},
+
+		amd: function(filename, mid){
+			return isJsRe.test(filename) && !(mid in amdExcludes);
+		}
+	}
+};
